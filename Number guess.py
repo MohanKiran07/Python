180c2affcdceed00961910d12023025afa2d0c87
Mohan Kiran <164997456+MohanKiran07@users.noreply.github.com>
2024-04-18 21:58:07 +0530

    Update Number guess.py

--- Number guess.py
+++ Number guess.py
@@ -1,21 +1,20 @@
-import random 
-def g(p,c):
-    if p==c:
-        return "u guess the number correct"
-    else :
-        return "u guess the number was worng"
+import random
+
+def compare_numbers(player_guess, computer_guess):
+    if player_guess == computer_guess:
+        return "Congratulations! You guessed the number correctly."
+    else:
+        return "Sorry, your guess was incorrect."
 
 while True:
-    p=input("guess a number between 1 to 5:")
-    c=['1','2','3','4','5']
-    c=random.choice(c)
-    result=g(p,c)
-    print(result)
+    player_input = input("Guess a number between 1 to 5: ")
+    computer_choices = ['1', '2', '3', '4', '5']
+    computer_guess = random.choice(computer_choices)
+    
+    result_message = compare_numbers(player_input, computer_guess)
+    print(result_message)
 
-
-
-
-
-
-
-
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() != 'yes':
+        print("Thanks for playing!")
+        break
