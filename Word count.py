fe7990835b2942a3e84eefcc5039b2fb699535e7
Mohan Kiran <164997456+MohanKiran07@users.noreply.github.com>
2024-04-18 22:02:32 +0530

    Update Word count.py

--- Word count.py
+++ Word count.py
@@ -1,8 +1,8 @@
-def c(w):
-    p=w.split()
-    c=len(p)
-    return c
-    
-w=input("enter words:")
-c=c(w)
-print(c)
+def count_words(input_string):
+    words_list = input_string.split()
+    word_count = len(words_list)
+    return word_count
+
+input_string = input("Enter a sentence or words: ")
+word_count = count_words(input_string)
+print("Number of words:", word_count)
