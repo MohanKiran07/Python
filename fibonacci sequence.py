7a22d3a05da2e418864078e1e0a8d94fcaa5c200
Mohan Kiran <164997456+MohanKiran07@users.noreply.github.com>
2024-04-18 22:03:57 +0530

    Update fibonacci sequence.py

--- fibonacci sequence.py
+++ fibonacci sequence.py
@@ -1,19 +1,18 @@
-def f(n):
-    fs=[0,1]
-    for i in range(2,n):
-        t=fs[i-1]+fs[i-2]
-        fs.append(t)
-    return fs
+def fibonacci_series(n):
+    # Initialize the Fibonacci series with the first two terms
+    fibonacci_sequence = [0, 1]
+    
+    # Generate Fibonacci series up to n terms
+    for i in range(2, n):
+        next_term = fibonacci_sequence[i-1] + fibonacci_sequence[i-2]
+        fibonacci_sequence.append(next_term)
+    
+    return fibonacci_sequence
 
-n=int(input("no of terms in fs:"))
-x=f(n)
-print("febinous Series:")
-print(x)
+# Get input from the user for the number of terms in the Fibonacci series
+num_terms = int(input("Enter the number of terms in the Fibonacci series: "))
 
-
-
-
-
-
-
-
+# Generate and print the Fibonacci series
+fib_series = fibonacci_series(num_terms)
+print("Fibonacci Series:")
+print(fib_series)
